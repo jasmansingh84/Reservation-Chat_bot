dcba815567dfc1ff51dcba002f16655a0f1aa6b4
Jasman <54865943+jasmansingh84@users.noreply.github.com>
2020-11-06 19:03:46 -0500

    Add files via upload
    Upload all JSON and javascript files

---
+++ RRBOT/rrbot.js
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler, MessageFactory } = require('botbuilder');
+
+class RRBOT extends ActivityHandler {
+    constructor() {
+        super();
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onMessage(async (context, next) => {
+            const replyText = `Echo: ${ context.activity.text }`;
+            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            await this.sendWelcomeMessage(context)
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+
+
+    async sendWelcomeMessage(turnContext) {
+        const { activity } = turnContext;
+    
+    //
+        for (const idx in activity.membersAdded) {
+            if (activity.membersAdded[idx].id !== activity.recipient.id) {
+                const welcomeMessage = `Welcome to Restaurant Reservation Bot ${ activity.memberAdded[idx].name }. `;
+                await turnContext.sendActivity(welcomeMessage);
+                await this.sendSuggestedActions(turnContext);
+            }
+        }   
+    }
+
+    async sendSuggestedActions(turnContext) {
+        var reply = MessageFactory.suggestedActions(['Make Reservation','Cancel Reservation','Restaurant Address'],'What would you like to do today ?');
+        await turnContext.sendActivity(reply);
+    }
+    
+
+}
+
+
+module.exports.RRBOT = RRBOT;
